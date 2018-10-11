45aa09e876c9adcba51b4f44d548082b7db1cc1a
Olexiy <olexiy.letushev@lun.ua>
2018-10-11 20:59:51 +0300

    initial cleanup, support scss and use css modules

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <h1>Hello, World!</h1>
+        <p className={styles.test}>Are SCSS and CSS modules working?</p>
+      </>
     );
   }
 }
